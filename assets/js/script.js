6069b307665973af1985b50b91e73c5ed714a1d4
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2017-06-14 09:00:28 -0400

    refactors listeners to handle event delegation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
 // Check off specific tasks by clicking 
 
-$("li").on("click", function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 // Delete list item when 'X' is clicked
-$('span').on("click", function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 });
 
+// Add new todo item to the list
 $("input[type='text']").on("keypress", function(event){
 	if(event.which === 13){
 		var newTodo = $(this).val();
